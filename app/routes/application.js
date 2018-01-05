8c39a89c436c92ffa717bbb33f0ae7efccaaee9d
Derek Wood <derekw@chromadex.com>
2018-01-04 23:16:34 -0800

    Adds Mirage + sends route to ‘accounts’

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  beforeModel() {
+    this.replaceWith('accounts');
+    // this.transitionTo('accounts');
+  },
+
 });
