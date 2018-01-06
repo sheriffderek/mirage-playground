e0d2a6277e5cefac3c6164221278f9dfae7b03ee
Derek Wood <derekw@chromadex.com>
2018-01-05 23:51:56 -0800

    Adds example computed properties on the ‘account’ model

--- app/models/account.js
+++ app/models/account.js
@@ -2,5 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  slug: Ember.computed('name', function() {
+    return this.get('name').dasherize();
+  }),
+
+  exampleComputedProperty: Ember.computed('name', 'slug', function() {
+    return this.get('name') + " → " + this.get('slug');
+  }),
+
   orders: DS.hasMany('order'),
 });
