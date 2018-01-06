0b6f8803b3a6e7feca06c9e81433ae1e1fc4a6cc
Derek Wood <derekw@chromadex.com>
2018-01-06 00:30:46 -0800

    Demonstrates bound data with input

--- app/models/account.js
+++ app/models/account.js
@@ -6,9 +6,9 @@ export default DS.Model.extend({
     return this.get('name').dasherize();
   }),
 
-  exampleComputedProperty: Ember.computed('name', 'slug', function() {
-    return this.get('name') + " → " + this.get('slug');
-  }),
-
   orders: DS.hasMany('order'),
+
+  placeOrder() {
+    console.log('placeOrder() from model');
+  },
 });
