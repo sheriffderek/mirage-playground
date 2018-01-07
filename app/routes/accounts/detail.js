c4e628146ba2706b03f9e310183cfe1823b9b4a1
Derek Wood <derekw@chromadex.com>
2018-01-07 10:56:56 -0800

    Adds peekRecord for detail page

--- app/routes/accounts/detail.js
+++ app/routes/accounts/detail.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 
-  model() {
-    // ?
+  model(urlParameters) { // these come from the dynamic segments
+    return this.store.peekRecord('account', urlParameters.id);
+    // use the id to match up with the dynamic segment and find the correct record in the store
   },
 });
