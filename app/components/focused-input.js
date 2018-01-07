39aaf60fdee97375750cdb3df964598ff2f3f31c
Derek Wood <derekw@chromadex.com>
2018-01-07 11:53:45 -0800

    Quick style

--- app/components/focused-input.js
+++ app/components/focused-input.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import $ from 'jquery';
 
 export default Component.extend({
+  tagName: 'div',
+  classNames: ['focused-input'],
 
   didInsertElement() {
     this.$('.focused').focus(); // $question - there must be a more elegant way.
