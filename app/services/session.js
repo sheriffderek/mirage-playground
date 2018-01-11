aafa8b94be699533f85e235fbb4e22aee38929b5
Derek Wood <derek@sheriffderek.consulting>
2018-01-11 09:21:48 -0800

    Adds ember-cookies + simple example

--- app/services/session.js
+++ app/services/session.js
@@ -1,14 +1,17 @@
 import Service from '@ember/service';
+import { inject as service } from '@ember/service';
 
 export default Service.extend({
 
-  currentUser: null,
+  cookies: service('cookies'),
 
   signIn(account) {
     this.set('currentUser', account)
+    this.get('cookies').write('currentUserId', account.id);
   },
 
   signOut() {
-    this.set('currentUser', null)
+    this.set('currentUser', null);
+    this.get('cookies').write('currentUserId', null);
   },
 });
