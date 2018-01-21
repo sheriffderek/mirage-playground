e973ad6e7780f5b6ec48096111024d5897ae6c63
Derek Wood <derek@sheriffderek.consulting>
2018-01-20 21:23:18 -0800

    Clears out changeset for a fresh branch

--- app/controllers/checkout/standard.js
+++ app/controllers/checkout/standard.js
@@ -4,17 +4,6 @@ export default Controller.extend({
 
   // Actions
   actions: {
-    submit(changeset) {
-      return changeset.save();
-    },
-
-    rollback(changeset) {
-      return changeset.rollback();
-    },
-
-    validate({ key, newValue, oldValue, changes, content }) {
-      // lookup a validator function on your favorite validation library
-      // should return a Boolean
-    },
+    //
   },
 });
