5c285e01f61341bd04dc2150a7ece981b0162810
Derek Wood <derek@sheriffderek.consulting>
2018-01-21 09:14:17 -0800

    beginings of cp component

--- app/components/forms/validatable-input.js
+++ app/components/forms/validatable-input.js
@@ -2,23 +2,31 @@ import Component from '@ember/component';
 import Ember from 'ember';
 
 export default Component.extend({
+
+  // Element
+  tagName: 'label',
   classNames: ['validated-input'],
   classNameBindings: ['showErrorClass:has-error', 'isValid:has-success'],
+
+  //
   model: null,
   value: null,
+
   type: 'text',
-  valuePath: '',
+  valuePath: '', // $question
   placeholder: '',
   validation: null,
   showValidations: false,
   didValidate: false,
 
+  //
   notValidating: Ember.computed.not('validation.isValidating').readOnly(),
   hasContent: Ember.computed.notEmpty('value').readOnly(),
   hasWarnings: Ember.computed.notEmpty('validation.warnings').readOnly(),
   isValid: Ember.computed.and('hasContent', 'validation.isTruelyValid').readOnly(),
   shouldDisplayValidations: Ember.computed.or('showValidations', 'didValidate', 'hasContent').readOnly(),
 
+  //
   showErrorClass: Ember.computed.and('notValidating', 'showErrorMessage', 'hasContent', 'validation').readOnly(),
   showErrorMessage: Ember.computed.and('shouldDisplayValidations', 'validation.isInvalid').readOnly(),
   showWarningMessage: Ember.computed.and('shouldDisplayValidations', 'hasWarnings', 'isValid').readOnly(),
