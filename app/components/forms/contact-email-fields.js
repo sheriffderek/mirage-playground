75c074f5629611d895304e44febe5d46d5758d09
Derek Wood <derek@sheriffderek.consulting>
2018-01-21 10:55:16 -0800

    Adds some UI updates for focus

--- app/components/forms/contact-email-fields.js
+++ app/components/forms/contact-email-fields.js
@@ -1,7 +1,11 @@
 import Component from '@ember/component';
-
+import $ from 'jquery';
 export default Component.extend({
 
   // Element
   tagName: 'fieldset',
+
+  didInsertElement() {
+    $('input.start-here').first().focus();
+  },
 });
--- app/components/forms/shipping-name-fields.js
+++ app/components/forms/shipping-name-fields.js
@@ -1,7 +1,12 @@
 import Component from '@ember/component';
+import $ from 'jquery';
 
 export default Component.extend({
 
   // Element
   tagName: 'fieldset',
+
+  didInsertElement() {
+    $('input.start-here').first().focus();
+  },
 });
