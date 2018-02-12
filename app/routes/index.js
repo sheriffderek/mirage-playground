dc6b187bc48869ded6dbc93071ab75ac4bcfeb02
Derek Wood <derek@sheriffderek.consulting>
2018-02-11 20:43:23 -0800

    Cleans up a bit - trying to figure out the surge error

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   beforeModel() {
-    this.replaceWith('product-detail');
+    // this.replaceWith('product-detail');
     // this.transitionTo('accounts');
   },
 
